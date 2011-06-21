c32e87358753084ef45d06542942d9484d12ca75
Lionel POINTET <lionel.pointet@globalis-ms.com>
2011-06-21 11:18:55 +0200

    Feature - Initialisation du projet git

---
+++ pack.sh
@@ -0,0 +1,222 @@
+#!/bin/bash
+function usage() {
+    printf "Usage %s: [-h] [-d] <tag_prod> <output_file> [git_rep] [tag_dev]\n" $(basename $0)
+    echo ""
+    echo "  -h              : Show this help"
+    echo "  -d              : Output a directory instead of an archive"
+    echo "  <tag_prod>      : The name of the tag with prod files"
+    echo "  <output_file>   : The name of the generated archive or directory"
+    echo "  [git_rep]       : The git repository file name (default: ./)"
+    echo "  [tag_dev]       : The name of the tag with development files (default: master)"
+    echo ""
+}
+
+#
+# paramètres :
+# $1 => output      nom de l'archive à créer
+# $2 => git_rep     répertoire git
+#
+function create_tar()
+{
+    if [ ${1%.*} = $1 ]
+    then
+        output=$1".tar.gz"
+    else
+        output=$1
+    fi
+    git_rep=$2
+    option=" --directory ${git_rep} "
+    expression=""
+
+    for line in $(cat $input)
+    do
+        echo ${git_rep}${line}
+        if [ -f ${git_rep}${line} ]
+        then
+            expression=${expression}" "$line
+        fi
+    done
+echo $expression
+    tar $option -czf $output $expression
+
+    if [ -f $output ]
+    then
+        echo "archive $output created!"
+    fi
+}
+#
+# paramètres :
+# $1 => input       nom du fichier listant les modifications
+# $2 => output      nom du répertoire dans lequel mettre les fichiers
+# $3 => git_rep     répertoire git
+#
+function create_dir()
+{
+    input=$1
+    output=$2
+    git_rep=$3
+
+    if [ ! -d $output ]
+    then
+        mkdir $output
+        chmod -R 777 $output
+    fi
+
+    for line in $(cat $input)
+    do
+        if [ -f ${git_rep}${line} ]
+        then
+            dir=${output}"/"${line%/*}
+            if [ ! -d $dir ]
+            then
+                mkdir -p $dir
+            fi
+            cp ${git_rep}${line} ${output}"/"${line}
+        fi
+    done
+
+    echo "directory created!"
+}
+#
+# paramètres :
+# $1 => tag_prod    tag git correspondant à l'image de la prod
+# $2 => git_rep     répertoire git
+# $3 => tag_dev     tag git correspondant au dev
+#
+# return : ouput    nom du fichier listant les modifications
+#
+function create_diff_file()
+{
+    tag_dev="master"
+    tag_prod=$1
+    git_rep=$2
+
+    dir_script=$(pwd)"/"
+    output="tmp.txt"
+
+    if [ ! $3 = "" ]
+    then
+        tag_dev=$3
+    fi
+
+    cd $git_rep
+    git diff $tag_dev $tag_prod --name-only > ${dir_script}${output}
+
+    if [ ! $tag_dev = "master" ]
+    then
+        git checkout $tag_dev --quiet
+    fi
+
+    cd $dir_script
+
+    echo $output
+}
+
+#
+# return : git_rep    racine vers le dépôt git
+#
+function get_git_rep()
+{
+    git_rep="$(git rev-parse --git-dir 2>/dev/null)"
+    git_rep="${git_rep%/.git}"
+    if [ ! $git_rep ]
+    then
+        git_rep="."
+    else
+        git_rep="${git_rep%.git}"
+        if [ ! $git_rep ]
+        then
+            git_rep="."
+        fi
+    fi
+    git_rep=$git_rep"/"
+
+    echo $git_rep
+}
+
+#
+# return : initial_tag_dev    tag/branche/commit sur lequel on se trouve en ce moment
+#
+function get_current_tag()
+{
+    if ! initial_tag_dev="$(git symbolic-ref HEAD 2>/dev/null)"
+    then
+        if ! initial_tag_dev="$(git describe --exact-match HEAD 2>/dev/null)"
+        then
+            initial_tag_dev="$(cut -c1-7 "$git_rep.git/HEAD")"
+        fi
+    fi
+    initial_tag_dev=${initial_tag_dev##refs/heads/}
+    
+    echo $initial_tag_dev
+}
+
+# Récupération des options
+TAR="TRUE"
+while getopts hd option
+do
+    case "${option}"
+    in
+        d)  TAR="FALSE";;
+        h)  usage
+            exit 0;;
+    esac
+done
+shift_arg=`expr $OPTIND - 1`
+shift $shift_arg
+
+# Récupération des paramètres
+if [ $# = 0 ]
+then
+    usage
+elif [ $# = 1 ]
+then
+    usage
+else
+    dir_script=$(pwd)"/"            # On retient le dossier dans lequel on est
+
+    # Récupération du chemin vers le dépôt
+    git_rep=$(get_git_rep)
+
+    tag_dev="master"
+
+    if [ -d $3 ] && [ ! $3 = "" ]   # $3 correspond au git_rep
+    then
+        cd $3
+        git_rep=$(pwd)"/"
+        cd $dir_script
+
+        if [ ! $4 = "" ]            # $4 est le tag_dev
+        then
+            tag_dev=$4
+        fi
+    elif [ ! $3 = "" ]              # $3 est le tag_dev
+    then
+        tag_dev=$3
+    fi
+
+    cd $git_rep
+    initial_tag_dev=$(get_current_tag)
+    cd $dir_script
+
+    input=$(create_diff_file ${1} ${git_rep} ${tag_dev})
+
+    if [ $TAR = "FALSE" ]
+    then
+        create_dir $input $2 $git_rep
+    else
+        create_tar $2 $git_rep
+    fi
+
+    if [ -f $input ]
+    then
+        rm $input
+    fi
+
+    if [ ! $tag_dev = $initial_tag_dev ]
+    then
+        cd $git_rep
+        git checkout $initial_tag_dev --quiet
+        cd $dir_script
+    fi
+fi
